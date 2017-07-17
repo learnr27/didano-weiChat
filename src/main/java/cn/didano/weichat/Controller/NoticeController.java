363d20f592e0457bda7c12753e7c492d60da2efb
guitar520 <574187554@qq.com>
2017-07-17 12:23:41 +0800

    消息通知

--- src/main/java/cn/didano/weichat/Controller/NoticeController.java
+++ src/main/java/cn/didano/weichat/Controller/NoticeController.java
@@ -2,7 +2,6 @@ package cn.didano.weichat.Controller;
 
 import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,6 +19,7 @@ import cn.didano.weichat.Service.NoticeService;
 import cn.didano.weichat.constant.BackType;
 import cn.didano.weichat.exception.ServiceException;
 import cn.didano.weichat.json.In_Notice_Edit;
+import cn.didano.weichat.json.In_Read_Date;
 import cn.didano.weichat.json.Out;
 import cn.didano.weichat.json.OutList;
 import cn.didano.weichat.model.Tb_notice;
@@ -36,6 +36,7 @@ public class NoticeController {
 	static Logger logger = Logger.getLogger(NoticeController.class);
 	@Autowired
 	private NoticeService noticeService;
+
 	/**
 	 * 发布通知
 	 *
@@ -46,59 +47,90 @@ public class NoticeController {
 	@PostMapping(value = "publish_notice")
 	@ResponseBody
 	public Out<String> publish_notice(
-			@ApiParam(value = "发布通知", required = true) @RequestBody In_Notice_Edit  notice_edit) {
+			@ApiParam(value = "发布通知", required = true) @RequestBody In_Notice_Edit notice_edit) {
 		logger.info("访问  NoticeController:publish_notice,notice_e=" + notice_edit);
-	
+
 		Tb_notice notice = null;
-		Tb_noticeUser noticeUser=null;
+		Tb_noticeUser noticeUser = null;
 		Out<String> back = new Out<String>();
-		
+
 		try {
 			notice = new Tb_notice();
 			BeanUtils.copyProperties(notice, notice_edit);
-			String content=notice_edit.getContent();
-			String content_preview=null;
-			//截取消息预览
-			if(content.length()>=20){
-				content_preview=content.substring(0, 20);
-			}else{
-				content_preview=content.substring(0);
+			String content = notice_edit.getContent();
+			String content_preview = null;
+			// 截取消息预览
+			if (content.length() >= 20) {
+				content_preview = content.substring(0, 20);
+			} else {
+				content_preview = content.substring(0);
 			}
 			notice.setContentPreview(content_preview);
 			int num = notice_edit.getUserId().size();
-			//判断用户类型
-			if(num==1){
-				//单个用户
-				notice.setPersonType((byte)1);
-			}else{
-			   //多个用户
-				notice.setPersonType((byte)2);
+			// 判断用户类型
+			if (num == 1) {
+				// 单个用户
+				notice.setPersonType((byte) 1);
+			} else {
+				// 多个用户
+				notice.setPersonType((byte) 2);
 			}
 			notice.setCreated(new Date());
-			//插入通知表
-		    noticeService.insertNoticeSelective(notice);
-		    int rowNum=0;
-		    //设置用户标记表
-		    for (int i = 0; i < num; i++) {
-				noticeUser=new Tb_noticeUser();
-				//默认未读
-				noticeUser.setIsRead((byte)0);
+			// 插入通知表
+			noticeService.insertNoticeSelective(notice);
+			int rowNum = 0;
+			// 设置用户标记表
+			for (int i = 0; i < num; i++) {
+				noticeUser = new Tb_noticeUser();
+				// 默认未读
+				noticeUser.setIsRead((byte) 0);
 				noticeUser.setNoticeId(notice.getId());
 				noticeUser.setUserId(notice_edit.getUserId().get(i));
 				noticeUser.setUserType(notice_edit.getUser_type());
 				noticeUser.setCreated(new Date());
 				noticeService.insertNoticeUserSelective(noticeUser);
 				rowNum++;
-			}		   
-					if (rowNum > 0) {
-						back.setBackTypeWithLog(BackType.SUCCESS_INSERT, "Id=" + "," + ":rowNum=" + rowNum);
+			}
+			if (rowNum > 0) {
+				back.setBackTypeWithLog(BackType.SUCCESS_INSERT, "Id=" + "," + ":rowNum=" + rowNum);
 
-					} else {
-						// 更新有问题
-						back.setBackTypeWithLog(BackType.FAIL_INSERT_NO_INSERT, "rowNum=" + rowNum);
-					}
-				 // end else
-			
+			} else {
+				// 更新有问题
+				back.setBackTypeWithLog(BackType.FAIL_INSERT_NO_INSERT, "rowNum=" + rowNum);
+			}
+			// end else
+
+		} catch (ServiceException e) {
+			// 服务层错误，包括 内部service 和 对外service
+			logger.warn(e.getMessage());
+			back.setServiceExceptionWithLog(e.getExceptionEnums());
+		} catch (Exception ex) {
+			logger.warn(ex.getMessage());
+			back.setBackTypeWithLog(BackType.FAIL_INSERT_NORMAL, ex.getMessage());
+		}
+		return back;
+	}
+
+	/**
+	 * 根据用户id,用户类型查找消息列表
+	 *
+	 * @throws ParseException
+	 * @throws InvocationTargetException
+	 * @throws IllegalAccessException
+	 */
+	@PostMapping(value = "notice_findtByUserid/{user_id}/{user_type}")
+	@ApiOperation(value = "根据用户id,用户类型查找消息列表", notes = "根据用户id,用户类型查找消息列表")
+	@ResponseBody
+	public Out<OutList<Tb_notice>> notice_findtByUserid(@PathVariable("user_id") Integer user_id,@PathVariable("user_type") byte user_type) {
+		logger.info("访问  NoticeController:notice_findtByUserid,user_id=" + user_id);
+
+		List<Tb_notice> notices = null;
+		OutList<Tb_notice> outList = null;
+		Out<OutList<Tb_notice>> back = new Out<OutList<Tb_notice>>();
+		try {
+			notices = noticeService.findNoticeByUserId(user_id,user_type);
+			outList = new OutList<Tb_notice>(notices.size(), notices);
+			back.setBackTypeWithLog(outList, BackType.SUCCESS_SEARCH_NORMAL);
 		} catch (ServiceException e) {
 			// 服务层错误，包括 内部service 和 对外service
 			logger.warn(e.getMessage());
@@ -110,34 +142,57 @@ public class NoticeController {
 		return back;
 	}
 	
-	
 	/**
-	 * 根据用户id查找消息列表
 	 *
-	 * @throws ParseException
-	 * @throws InvocationTargetException
-	 * @throws IllegalAccessException
+	 * 删除通知消息
+	 *
+	 * @param teacher_id
+	 * @return
 	 */
-	@PostMapping(value = "notice_findtByUserid/{user_id}")
-	@ApiOperation(value = "根据用户id查找消息列表", notes = "根据用户id查找消息列表")
+	@PostMapping(value = "deleteNotice/{notice_id}")
+	@ApiOperation(value = " 删除通知消息", notes = " 删除通知消息")
 	@ResponseBody
-	public Out<OutList<Tb_notice>> notice_findtByUserid(@PathVariable("user_id") Integer user_id) {
-		logger.info("访问  NoticeController:notice_findtByUserid,user_id=" + user_id);
-
-		List<Tb_notice> notices = null;
-		OutList<Tb_notice> outList = null;
-		Out<OutList<Tb_notice>> back = new Out<OutList<Tb_notice>>();
+	public Out<String> deleteStaff(@PathVariable("notice_id") Integer notice_id) {
+		logger.info("访问  NoticeController:deleteNotic,notice_id=" + notice_id);
+		Out<String> back = new Out<String>();
 		try {
-			notices = noticeService.findNoticeByUserId(user_id);
-			outList = new OutList<Tb_notice>(notices.size(), notices);
-			back.setBackTypeWithLog(outList, BackType.SUCCESS_SEARCH_NORMAL);
+			noticeService.deleteNoticeById(notice_id);
+			int rowNum=noticeService.deleteNoticeUserByNoticeId(notice_id);
+			if (rowNum > 0) {
+				back.setBackTypeWithLog(BackType.SUCCESS_UPDATE, "rowNum=" + rowNum);
+			} else {
+				back.setBackTypeWithLog(BackType.FAIL_UPDATE_NORMAL, "rowNum=" + rowNum);
+			}
 		} catch (ServiceException e) {
-			// 服务层错误，包括 内部service 和 对外service
 			logger.warn(e.getMessage());
 			back.setServiceExceptionWithLog(e.getExceptionEnums());
-		} catch (Exception ex) {
-			logger.warn(ex.getMessage());
-			back.setBackTypeWithLog(BackType.FAIL_INSERT_NORMAL, ex.getMessage());
+		}
+		return back;
+	}
+	
+	/**
+	 *
+	 * 设置消息已读
+	 *
+	 * @param teacher_id
+	 * @return
+	 */
+	@PostMapping(value = "setNoticeRead")
+	@ApiOperation(value = " 删除通知消息", notes = " 删除通知消息")
+	@ResponseBody
+	public Out<String> setNoticeRead(@RequestBody In_Read_Date date) {
+		logger.info("访问  NoticeController:setNoticeRead,date=" + date);
+		Out<String> back = new Out<String>();
+		try {
+			int rowNum=noticeService.setNoticeRead(date.getUserId(), date.getNoticeId());
+			if (rowNum > 0) {
+				back.setBackTypeWithLog(BackType.SUCCESS_UPDATE, "rowNum=" + rowNum);
+			} else {
+				back.setBackTypeWithLog(BackType.FAIL_UPDATE_NORMAL, "rowNum=" + rowNum);
+			}
+		} catch (ServiceException e) {
+			logger.warn(e.getMessage());
+			back.setServiceExceptionWithLog(e.getExceptionEnums());
 		}
 		return back;
 	}
--- src/main/java/cn/didano/weichat/Service/NoticeService.java
+++ src/main/java/cn/didano/weichat/Service/NoticeService.java
@@ -1,6 +1,7 @@
 package cn.didano.weichat.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import cn.didano.weichat.dao.Tb_noticeUserMapper;
 import cn.didano.weichat.exception.DBExceptionEnums;
 import cn.didano.weichat.exception.ServiceException;
 import cn.didano.weichat.model.Tb_notice;
+import cn.didano.weichat.model.Tb_noticeExample;
 import cn.didano.weichat.model.Tb_noticeUser;
 import cn.didano.weichat.model.Tb_noticeUserExample;
 
@@ -43,21 +45,85 @@ public class NoticeService {
 	/**
 	 * 根据用户id查找消息列表
 	 */
-	public List<Tb_notice> findNoticeByUserId(Integer id){
+	public List<Tb_notice> findNoticeByUserId(Integer id,byte type){
 		Tb_noticeUserExample condition = new Tb_noticeUserExample();
 		Tb_noticeUserExample.Criteria criteria = condition.createCriteria();
          //对于已经deleted=1的不显示 禁用不显示
  		criteria.andUserIdEqualTo(id);
+ 		criteria.andDeletedEqualTo(false);
+ 		criteria.andUserTypeEqualTo(type);
  		condition.setOrderByClause("created");
  		List<Tb_noticeUser> noticeUser= noticeUserMapper.selectByExample(condition);
  		List<Tb_notice> notices = new ArrayList<Tb_notice>();
+ 		List<Integer> priority=new ArrayList<Integer>();
  		Tb_notice notice = null;
+ 		
+ 		//查询出置顶的消息
+ 		for (int i = 0; i < noticeUser.size(); i++) {
+ 			notice = noticeMapper.selectByPrimaryKey(noticeUser.get(i).getNoticeId());
+ 			if(notice.getPriority()==1){
+ 				priority.add(i);
+ 			}
+		}
+ 		for (int i = 0; i < priority.size(); i++) {
+ 			notice = noticeMapper.selectByPrimaryKey(noticeUser.get(i).getNoticeId());
+ 			notices.add(notice);
+ 			//去除已经置顶的消息
+ 			noticeUser.remove(i);
+		}
+ 		
  		//按时间排序
  		for (int i = noticeUser.size()-1; i >=0 ; i--) {
 			notice = noticeMapper.selectByPrimaryKey(noticeUser.get(i).getNoticeId());
 			notices.add(notice);
 		}
+ 		//倒序
+ 		Collections.reverse(notices);
  		return notices;
 	}
+	/**
+	 * 根据通知id删除该通知消息下的用户消息
+	 */
+	public int deleteNoticeUserByNoticeId(Integer noticeId){
+		Tb_noticeUserExample condition = new Tb_noticeUserExample();
+		Tb_noticeUserExample.Criteria criteria = condition.createCriteria();
+		criteria.andNoticeIdEqualTo(noticeId);
+		criteria.andDeletedEqualTo(false);
+		List<Tb_noticeUser> noticeUsers = noticeUserMapper.selectByExample(condition);
+		Tb_noticeUser noticeUser = null;
+		int row=0;
+		for (int i = 0; i <noticeUsers.size(); i++) {
+			noticeUser = noticeUsers.get(i);
+			noticeUser.setDeleted(true);
+			noticeUserMapper.updateByPrimaryKeySelective(noticeUser);
+			row++;
+		}
+		return row;
+				
+	}
 	
+	/**
+	 * 通告通知id删除通知信息
+	 */
+	public int deleteNoticeById(Integer id){
+		Tb_notice notice = noticeMapper.selectByPrimaryKey(id);
+		notice.setDeleted(true);
+		return noticeMapper.updateByPrimaryKeySelective(notice);
+	}
+	
+	/**
+	 * 设置消息已读
+	 */
+	public int setNoticeRead(Integer userId,Integer noticeId){
+		Tb_noticeUserExample condition = new Tb_noticeUserExample();
+		Tb_noticeUserExample.Criteria criteria = condition.createCriteria();
+         //对于已经deleted=1的不显示 禁用不显示
+ 		criteria.andUserIdEqualTo(userId);
+ 		criteria.andNoticeIdEqualTo(noticeId);
+ 		criteria.andDeletedEqualTo(false);
+ 		List<Tb_noticeUser> noticeUser = noticeUserMapper.selectByExample(condition);
+ 		Tb_noticeUser user = noticeUser.get(0);
+ 		user.setIsRead((byte)1);
+ 		return noticeUserMapper.updateByPrimaryKeySelective(user);
+	}
 }
