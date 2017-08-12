50cd13818049896a17e18f9000385f3d1e98c22e
underhill6 <Underhill.chen@didano.cn>
2017-08-12 14:40:40 +0800

    更新NoticeService...

--- src/main/java/cn/didano/weichat/Service/NoticeService.java
+++ src/main/java/cn/didano/weichat/Service/NoticeService.java
@@ -163,6 +163,7 @@ public class NoticeService {
 		condition.setOrderByClause("created");
 		List<Tb_noticeUser> noticeUser = noticeUserMapper.selectByExample(condition);
 		List<Tb_notice> notices = new ArrayList<Tb_notice>();
+		if(notices.size()!=0) {
 		Tb_notice notice = null;
 
 		// 查询出置顶的消息
@@ -190,7 +191,7 @@ public class NoticeService {
 			notice.setIs_read(noticeUser.get(i).getIsRead());
 			notices.add(notice);
 		}
-
+		}
 		return notices;
 	}
 
